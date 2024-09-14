56fcc14d035cf0dbb3ffb8bcc615570779a392c7
Tyuiu.KulkoDA@outlook.com <Tyuiu.KulkoDA@outlook.com>
2024-09-14 21:44:07 +0500

    Task3

---
+++ Tyuiu.KulkoDA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.KulkoDA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.KulkoDA.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7,7));
+            Console.ReadLine();
+        }
+    }
+}
