3581ebe7560e7a5f623cae0f21b0766ab5a8c015
Tyuiu.KulkoDA@outlook.com <Tyuiu.KulkoDA@outlook.com>
2024-09-14 21:14:29 +0500

    Task2

---
+++ Tyuiu.KulkoDA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.KulkoDA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KulkoDA.Sprint0.Task2.V0
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Дарья"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
